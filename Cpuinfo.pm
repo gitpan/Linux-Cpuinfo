451210f3591230f1a508b72b155374c33551502d
Jonathan Stowe <jns@gellyfish.co.uk>
2001-09-11 08:04:58 +0000

    Import of JSTOWE/Linux-Cpuinfo-1.3 from CPAN.
    gitpan-cpan-distribution: Linux-Cpuinfo
    gitpan-cpan-version:      1.3
    gitpan-cpan-path:         JSTOWE/Linux-Cpuinfo-1.3.tar.gz
    gitpan-cpan-author:       JSTOWE
    gitpan-cpan-maturity:     released

--- Cpuinfo.pm
+++ Cpuinfo.pm
@@ -14,6 +14,12 @@
 #*****************************************************************************
 #*
 #*          $Log: Cpuinfo.pm,v $
+#*          Revision 1.3  2001/09/11 07:23:16  gellyfish
+#*          * Applied kindly patch from Tels
+#*          * Added proper SMP support
+#*          * Test for SMP support
+#*
+#*
 #*          Revision 1.2  2001/06/17 21:28:26  gellyfish
 #*          Ooh forgot the documentation
 #*
@@ -32,23 +38,49 @@ Linux::Cpuinfo - Object Oriented Interface to /proc/cpuinfo
 
 =head1 SYNOPSIS
 
+  # Old interface ( for single processor devices )
+
   use Linux::Cpuinfo;
 
   my $cpu = Linux::Cpuinfo->new();
 
+  die ('Could not find cpu info (does /proc/cpuinfo exists?)')
+    unless ref $cpu;
+
   print $cpu->model_name();
+
+
+  # New Interface ( copes with SMP ).
+
+  my $cpuinfo = Linux::Cpuinfo->new();
+
+  $cnt  = $cpuinfo->num_cpus();	 # > 0 for an SMP system
+
+
+   foreach my $cpu ( $cpuinfo->cpus() )
+   {
+     print $cpu->bogomips(),"\n";
+   }
 
 =head1 DESCRIPTION
 
-On Linux systems various information about the CPU in the computer can
-be gleaned from C</proc/cpuinfo>.  This module provides an object oriented
-interface to that information for relatively simple use in Perl programs.
+On Linux systems various information about the CPU ( or CPUs ) in the
+computer can be gleaned from C</proc/cpuinfo>. This module provides an
+object oriented interface to that information for relatively simple use
+in Perl programs.
 
 =head2 METHODS
 
-It is entirely probable that all of the methods will be different on other
-processors than an x86 - I would hope for feedback from users of other
-processors to make this complete.
+The interface has changed between revisions 1.2 and 1.3 of this module
+in order to deal with systems with multiple CPUs - now the details of a
+CPU are acquired by the methods of the Linux::Cpuinfo::Cpu objects returned
+by the C<cpu()> and C<cpus()> methods of this class. However in order to
+retain backward compatibility if the methods described for Linux::Cpuinfo::Cpu
+are called a Linux::Cpuinfo object then it will work as previously - returning
+the details of the sole CPU on a single processor system and the last discovered
+ CPU on system with multiple processors ( this was the implicit behaviour on
+previous versions).  Whilst not strictly deprecated this interface is not
+the recommended one.
 
 =over 4
 
@@ -65,7 +97,7 @@ use vars qw(
              $AUTOLOAD
            );
 
-($VERSION) = q$Revision: 1.2 $ =~ /([\d.]+)/;
+($VERSION) = q$Revision: 1.3 $ =~ /([\d.]+)/;
 
 =item cpuinfo
 
@@ -85,34 +117,51 @@ sub cpuinfo
 
    $file ||= '/proc/cpuinfo';
    
-   if ( -e $file  )
+   if ( -e $file   and  -f $file  )
    {
     
       if ( open(CPUINFO,$file ) )
       {
          $self = {};
 
+         my $ofh = select CPUINFO ; $/ = ''; select $ofh;
+
+         $self->{_private}->{num_cpus} = 0;
+
+         $self->{_cpuinfo} = [];
+
          while(<CPUINFO>)
          {
             chomp;
 
-            my ( $attribute, $value ) = split /\s*:\s*/;
+            $self->{_private}->{num_cpus}++;
 
-            $attribute =~ s/\s+/_/;
+            my $cpuinfo = {};
 
-            if ( $value =~ /^(no|yes)$/ )
+            foreach my $cpuline ( split /\n/ )
             {
-               $value = $value eq 'yes' ? 1 : 0;
-            }
+               my ( $attribute, $value ) = split /\s*:\s*/, $cpuline;
+
+               $attribute =~ s/\s+/_/;
+               $attribute = lc($attribute);
+
+               if ( $value =~ /^(no|not available|yes)$/ )
+               {
+                  $value = $value eq 'yes' ? 1 : 0;
+               }
             
-            if ( $attribute eq 'flags' )
-            {
-               @{$self->{flags}} = split / /, $value;
+               if ( $attribute eq 'flags' )
+               {
+                  @{$cpuinfo->{flags}} = split / /, $value;
+               }
+               else
+               {
+                 $cpuinfo->{$attribute} = $value;
+               }
+
             }
-            else
-            {
-               $self->{$attribute} = $value;
-            }
+            bless $cpuinfo, "${class}::Cpu";
+            push @{$self->{_cpuinfo}}, $cpuinfo;
          }
 
          bless $self, $class;
@@ -126,22 +175,125 @@ sub cpuinfo
 
 *new = \&cpuinfo;
 
+=item num_cpus
+
+Returns the number of CPUs reported for this system.  
+
+=cut
+
+sub num_cpus
+{
+   my ( $self ) = @_;
+
+   return $self->{_private}->{num_cpus};
+}
+
+=item cpu SCALAR $cpu
+
+Returns an object of type Linux::Cpuinfo::Cpu corresponding to the CPU of
+index $cpu ( where $cpu >= 0 and $cpu < num_cpus() ) - if $cpu is omitted
+this will return an object correspnding to the last CPU found.
+
+If $cpu is out of bounds with respect to the number of CPUs then it will
+be set to the first or last CPU ( depending whether $cpu was < 0 or >num_cpus )
+
+=cut
+
+sub cpu
+{
+   my ( $self, $cpu ) = @_;
+
+   if ( defined $cpu )
+   {
+     $cpu = 0 if ( $cpu < 0 );
+     $cpu = $#{$self->{_cpuinfo}} if $cpu > $#{$self->{_cpuinfo}}; 
+   }
+   else
+   {
+     $cpu = $#{$self->{_cpuinfo}};
+   }
+
+   return $self->{_cpuinfo}->[$cpu];
+}
+
+
+=item cpus
+
+Returns a list containing objects of type Linux::Cpuinfo::Cpu corresponding 
+to the CPUs discovered in this system.   If the method is called in a scalar
+context it will return a reference to an array of those objects.
+
+=cut
+
+
+sub cpus
+{
+   my ( $self ) = @_;
+
+   if ( wantarray() )
+   {
+      return @{$self->{_cpuinfo}};
+   }
+   else
+   {
+      return $self->{_cpuinfo};
+   }
+}
+
+sub AUTOLOAD
+{
+
+  my ( $self ) = @_;
+
+  return if $AUTOLOAD =~ /DESTROY/;
+
+  my ( $method ) = $AUTOLOAD =~ /.*::(.+?)$/;
+
+  no strict 'refs';
+
+  *{$AUTOLOAD} = sub {
+                       my ($self) = @_;
+                       return $self->{_cpuinfo}->[$#{$self->{_cpuinfo}}]->$method();
+                     };
+  goto &{$AUTOLOAD};
+
+  use strict 'refs';
+}
+
+# The following are autoloaded methods of the Linux::Cpuinfo::Cpu class
+
+=back
+
+=head2 PER CPU METHODS OF Linux::Cpuinfo::Cpu
+
+Note that not all of the methods listed here are available on all CPU
+types.  For instance, MIPS CPUs have no cpuid instruction, but might
+sport a byte order attribute.
+
+There are also some other methods available for some CPUs which aren't
+listed here.
+
+=over 4
+
 =item processor	
 
-As far as I know this is the index of the processor this information is for
-this will be zero for a single processor system. Someone might want to tell
-me what it is for multiple CPUs
+This is the index of the processor this information is for, it will be zero
+for a the first CPU (which is the only one on single-proccessor systems), one
+for the second and so on.
 
 =item vendor_id	
 
-This is a vendor defined string such as 'GenuineIntel'
+This is a vendor defined string for X86 CPUs such as 'GenuineIntel' or
+'AuthenticAMD'. 12 bytes long, since it is returned via three 32 byte long
+registers.
 
 =item cpu_family	
 
 This should return an integer that will indicate the 'family' of the 
-processor - This is for instance '6' for a Pentium III
+processor - This is for instance '6' for a Pentium III. Might be undefined for
+non-X86 CPUs.
 
-=item model		
+=item model or cpu_model
 
 An integer that is probably vendor dependent that indicates their version 
 of the above cpu_family
@@ -154,10 +306,15 @@ A string such as 'Pentium III (Coppermine)'.
 
 I'm lead to believe this is a version increment used by intel.
 
-=item cpu_MHz		
+=item cpu_mhz
 
 I guess this is self explanatory - it might however be different to what
-it says on the box.
+it says on the box. The Mhz is measured at boot time by the kernel and
+represents the true Mhz at that time.
+
+=item bus_mhz
+
+The MHz of the bus system.
 
 =item cache_size	
 
@@ -192,31 +349,49 @@ True if the CPU has a floating point unit.
 
 True if the floating point unit can throw an exception.
 
-=item cpuid_level	
+=item cpuid_level
 
-I'm not sure what this is.
+The C<cpuid> assembler instruction is only present on X86 CPUs. This attribute
+represents the level of the instruction that is supported by the CPU. The first
+CPUs had only level 1, newer chips have more levels and can thus return more
+information.
 
-=item wp		
+=item wp
 
-Or this.
+No idea what this is on X86 CPUs.
 
-=item flags		
+=item flags
 
 This is the set of flags that the CPU supports - this is returned as an
 array reference.
 
+=item byte_order
+
+The byte order of the CPU, might be little endian or big endian, or undefined
+for unknown.
+
 =item bogomips	
 
-A system constant calculated when the kernel is booted - it is a measure
-of the CPU's performance.
+A system constant calculated when the kernel is booted - it is a (rather poor)
+measure of the CPU's performance.
+
+=back
 
 =cut
 
+
+package Linux::Cpuinfo::Cpu;
+
+use strict;
+
+use vars qw($AUTOLOAD);
 
 sub AUTOLOAD
 {
 
   my ( $self ) = @_;
+
+  return if $AUTOLOAD =~ /DESTROY/;
 
   my ( $method ) = $AUTOLOAD =~ /.*::(.+?)$/;
 
@@ -234,9 +409,11 @@ sub AUTOLOAD
   }
   else
   {
-    croak sprintf(q(Can't locate object method "%s" via package "%s"),
+
+    croak( sprintf(q(Can't locate object method "%s" via package "%s"),
                   $method,
-                  ref($self)); 
+                  ref($self))); 
+
 
   }
 }
--- test.pl
+++ test.pl
@@ -1,13 +1,25 @@
 use Test;
-BEGIN { plan tests => 3 };
+BEGIN { plan tests => 10 };
 use Linux::Cpuinfo;
 ok(1); 
 
-my $cpu;
+my $cpuinfo;
 eval
 {
-   $cpu = Linux::Cpuinfo->cpuinfo();
+   $cpuinfo = Linux::Cpuinfo->cpuinfo();
    ok(2);
+};
+if ($@)
+{
+  ok(0);
+}
+
+# Test old interface.
+
+eval
+{
+   my $bog = $cpuinfo->bogomips();
+   ok(3);
 };
 if ($@)
 {
@@ -15,10 +27,77 @@ if ($@)
 }
 eval
 {
-   my $bog = $cpu->bogomips();
-   ok(3);
+  $cpuinfo = Linux::Cpuinfo->new('proc/cpuinfo.x86_smp');
+  ok(4);
 };
-if ($@)
+if ( $@)
+{
+  ok(0);
+}
+eval
+{
+  my $num_cpus = $cpuinfo->num_cpus();
+  ok(5);
+  
+  if ( $num_cpus != 2 )
+  {
+    print $num_cpus,"\n";
+    ok(0);
+  }
+  else
+  {
+    ok(6);
+  }
+};
+if ( $@ )
+{
+  ok(0);
+  ok(0);
+}
+
+# test the new interface.
+
+my $cpu;
+
+eval
+{
+ $cpu = $cpuinfo->cpu(0);
+  ok(7);
+};
+if ( $@ )
+{
+  ok(0);
+}
+    
+eval 
+{
+  my $chip = $cpu->model_name();
+  ok(8);
+
+  if ( $chip ne 'Pentium Pro' )
+  {
+    ok(0);
+  }
+  else
+  {
+    ok(9);
+  }
+};
+if ($@ )
+{
+  ok(0);
+  ok(0);
+}
+
+eval
+{
+   foreach $cpu ( $cpuinfo->cpus() )
+   {
+     my $bog = $cpu->bogomips();
+   }
+   ok(10);
+};
+if ( $@ )
 {
   ok(0);
 }
